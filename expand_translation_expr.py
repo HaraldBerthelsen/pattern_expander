f80f4d46d425755aa7535798870266fa44883e28
Harald Berthelsen <harald@stts.se>
2016-05-17 14:51:10 +0200

    hb

--- expand_translation_expr.py
+++ expand_translation_expr.py
@@ -21,11 +21,28 @@ def generate(regexp):
         #sys.exit()
     
     #print "Generated %d string(s)" % len(gens)
+
+    #build a dictionary to check for doubles
+    gen_count = {}
+    for gen in gens:    
+        gen = gen.strip()
+        gen = re.sub(" +"," ",gen)
+        if gen in gen_count:
+            gen_count[gen] += 1
+        else:
+            gen_count[gen] = 1
+
+
+
     mod_gens = []
     for gen in gens:    
         gen = gen.strip()
         gen = re.sub(" +"," ",gen)
+        if gen_count[gen] > 1:
+            mod_gens.append("WARNING --- DOUBLE %d" % gen_count[gen])
         mod_gens.append(gen)
+
+
     return mod_gens
 
 
@@ -46,9 +63,32 @@ def read_stdin():
     no = 0
     for line in lines:
         line = line.strip()
+
+        #A way to comment lines if needed
+        if line.startswith("//"):
+            continue
+
+
         no = no+1
-        if re.search("[a-zA-Z]", line):
-            print("WARNING: Latin characters in line %d: %s" % (no, line))
+        #if re.search("[a-zA-Z]", line):
+        #    print("WARNING: Latin characters in line %d: %s" % (no, line))
+
+        #number each "esh esh esh" in persian
+        i = 0
+        esh = r"ششش"
+
+        if re.search(esh+".*"+esh, line):
+            #sys.stderr.write("A\n")
+            #sys.stderr.write(line+"\n")
+
+            while re.search(esh+"[^0-9]", line):
+                line = re.sub("("+esh+r")([^0-9])", r"\g<1>"+str(i)+r"\g<2>", line, count=1) 
+                i += 1
+
+            #sys.stderr.write("B\n")
+            #sys.stderr.write(line+"\n")
+            #sys.exit()
+
         gens = generate(line)
         print("%d\n%s\n%d string(s)\n" % (no,line,len(gens)))
         for gen in gens:
